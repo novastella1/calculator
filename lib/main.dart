3713a9293b0509e112f62f08dacab0261e2dddf3
Ebenezer <72541522+novastella1@users.noreply.github.com>
2022-01-28 01:02:20 -0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'buttonlayout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ButtonLayout(),
+    );
+  }
+}
